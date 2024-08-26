b6bad3fb6c0d410ee6c25c980f3061f802867112
Alok Singh <mailbox.alok.singh.11@gmail.com>
2024-08-27 00:12:11 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Todo from './components/Todo'
+import React from "react";
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
-    <div className='bg-stone-900 grid py-4 min-h-screen'>
-      <Todo/>
+    <div className="bg-stone-900 grid py-4 min-h-screen">
+      <Todo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
